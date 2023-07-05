de979b9883d0d287f702b946a1a5f2a3e6a8785f
Tristan Anh Ngo <54490272+tristanhuyanhngo@users.noreply.github.com>
2023-07-05 21:15:20 +0700

    Completed Todolist with React State & React Hooks
    Done

--- Project/ReactJS_Todolist/src/App.jsx
+++ Project/ReactJS_Todolist/src/App.jsx
@@ -1,14 +1,57 @@
 import "./styles.css";
+import { useEffect, useState } from "react";
+import { TodoList } from "./TodoList";
+import { NewTodoForm } from "./NewTodoForm";
 
 function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+    console.log(localValue)
+    return JSON.parse(localValue)
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos));
+  }, [todos]);
+
+  function addTodo(title) {
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title,
+          completed: false,
+        },
+      ];
+    });
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      console.log(todos);
+      return currentTodos.filter((todo) => todo.id !== id); // Create a new array excludes the todo item with the given id.
+    })
+  }
+
   return (
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item"></input>
-      </div>
-      <button className="btn">Add</button>
-    </form>
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo}/>
+    </>
   );
 }
 
---
+++ Project/ReactJS_Todolist/src/TodoList.jsx
@@ -0,0 +1,25 @@
+import { TodoItem } from "./TodoItem";
+import PropTypes from "prop-types";
+
+export function TodoList({todos, toggleTodo, deleteTodo}) {
+    return (
+      <ul className="list">
+        {todos.length === 0 && "No Todos"}
+        {todos.map(todo => {
+          return (
+            <TodoItem {...todo} 
+            key={todo.id} 
+            toggleTodo={toggleTodo} 
+            deleteTodo={deleteTodo}
+            />
+          );
+        })}
+      </ul>
+    );
+}
+
+TodoList.propTypes = {
+  todos: PropTypes.bool.isRequired,
+  toggleTodo: PropTypes.bool.isRequired,
+  deleteTodo: PropTypes.bool.isRequired
+};
