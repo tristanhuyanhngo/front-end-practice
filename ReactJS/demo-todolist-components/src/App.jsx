7002b31c58e97a3900228e59c79e144e830c310d
Tristan Anh Ngo <54490272+tristanhuyanhngo@users.noreply.github.com>
2023-07-09 19:47:13 +0700

    Structure Todolist App & Rendering Data
    Done

---
+++ ReactJS/demo-todolist-components/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TodoFeature from "./features/Todo";
+
+function App() {
+  return (
+    <div className="App">
+        <TodoFeature />
+    </div>
+  )
+}
+
+export default App
