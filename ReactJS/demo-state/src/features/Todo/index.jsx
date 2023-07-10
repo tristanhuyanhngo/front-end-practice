0f2d53101171bebd209aaa79cbf3487e8308a654
Tristan Anh Ngo <54490272+tristanhuyanhngo@users.noreply.github.com>
2023-07-10 15:24:21 +0700

    Demo filter Todolist
    Done

--- ReactJS/demo-state/src/features/Todo/index.jsx
+++ ReactJS/demo-state/src/features/Todo/index.jsx
@@ -21,6 +21,7 @@ function TodoFeature() {
   ];
 
   const [todoList, setTodoList] = useState(initTodoList);
+  const [filteredStatus, setFilteredStatus] = useState("all");
 
   const handleTodoClick = (todo, idx) => {
     // Clone current array to the new one
@@ -38,10 +39,30 @@ function TodoFeature() {
     setTodoList(newTodoList);
   }
 
+  const handleShowAllClick = () => {
+    setFilteredStatus("all");
+  }
+
+  const handleShowCompletedClick = () => {
+    setFilteredStatus("completed");
+  }
+
+  const handleShowNewClick = () => {
+    setFilteredStatus("new");
+  }
+
+  const renderedTodoList = todoList.filter(todo => filteredStatus === "all" || filteredStatus === todo.status)
+  console.log(renderedTodoList);
+
   return (
     <div>
       <h3>Todo List</h3>
-      <TodoList todoList={todoList} onTodoClick={handleTodoClick} />
+      <TodoList todoList={renderedTodoList} onTodoClick={handleTodoClick} />
+      <div>
+        <button onClick={handleShowAllClick}>Show All</button>
+        <button onClick={handleShowCompletedClick}>Show Completed</button>
+        <button onClick={handleShowNewClick}>Show New</button>
+      </div>
     </div>
   )
 }
